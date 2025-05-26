e847159c3395ef0f6e94bade5309ec9e922d1903
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-05-26 14:52:10 -0300

    feat: modal

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+const modal = document.getElementById("modal");
+const menu = document.getElementById("menu");
+const cartButton = document.getElementById("cart-button");
+const cartAdd = document.querySelectorAll("svg")
+
